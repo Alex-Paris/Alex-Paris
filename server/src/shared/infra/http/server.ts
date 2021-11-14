eb3ced54220261c514f3dd5d81d26c17fc54e027
Alex Paris <alexlima@gruposgapetro.com.br>
2021-11-14 18:36:26 -0300

    chore-server: app error

---
+++ server/src/shared/infra/http/server.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express';
+
+import AppError from 'shared/errors/AppError';
+
+const app = express();
+
+app.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('👻 Buuu! Server started on port 3333!');
+});
