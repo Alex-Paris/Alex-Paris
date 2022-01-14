e9a76e2c67439a3da6dbf2e57ec280414d0aa123
Alex Paris <alexlima@gruposgapetro.com.br>
2022-01-14 13:49:30 -0300

    chore-client: dash mob page barbers list

--- client/src/demos/GoBarber/mobile/Dashboard/index.tsx
+++ client/src/demos/GoBarber/mobile/Dashboard/index.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import 'react-day-picker/lib/style.css';
-
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiCalendar, FiClock } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/context/auth';
+import { useMobileRoute } from '../../../../hooks/context/mobileRoute';
+
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  ProvidersContainer,
+  ProvidersList,
+  ProvidersMeta,
+} from './styles';
+import { useStorage } from '../../hooks/context/storage';
 
 const Dashboard: React.FC = () => {
   const { signOutMobile, userMobile } = useAuth();
+  const { togglePage } = useMobileRoute();
+  const { users } = useStorage();
 
-  let tmpUser = userMobile;
-
-  if (!tmpUser) {
-    tmpUser = {
-      id: '0',
-      name: 'John Doe',
-      email: 'johndoe@email.com',
-      avatar_url: '',
-    };
-  }
+  const handleSignOut = useCallback(() => {
+    signOutMobile();
+    togglePage('SignIn');
+  }, [signOutMobile, togglePage]);
 
   return (
     <Container>
@@ -27,26 +34,54 @@ const Dashboard: React.FC = () => {
           <Profile>
             <div>
               <span>Welcome,</span>
-              <strong>{tmpUser.name}</strong>
+              <strong>{userMobile.name}</strong>
             </div>
           </Profile>
-          <button type="button" onClick={signOutMobile}>
-            Sair
+          <button type="button" onClick={handleSignOut}>
+            Logout
           </button>
 
           <Link to="/demos/GoBarber/profile">
             <img
               src={
-                tmpUser.avatar_url
-                  ? tmpUser.avatar_url
+                userMobile.avatar_url
+                  ? userMobile.avatar_url
                   : 'http://cdn.onlinewebfonts.com/svg/img_261106.png'
               }
-              alt={tmpUser.name}
+              alt={userMobile.name}
             />
           </Link>
         </HeaderContent>
       </Header>
-      <div>Barbers</div>
+      <ProvidersList>
+        <p>Barbers</p>
+
+        {users.map((barber) => (
+          <ProvidersContainer key={barber.id}>
+            <img
+              src={
+                barber.avatar_url
+                  ? barber.avatar_url
+                  : 'http://cdn.onlinewebfonts.com/svg/img_261106.png'
+              }
+              alt={barber.name}
+            />
+
+            <div>
+              <strong>{barber.name}</strong>
+              <ProvidersMeta>
+                <FiCalendar size={14} color="ff9000" />
+                <p>Monday to Friday</p>
+              </ProvidersMeta>
+
+              <ProvidersMeta>
+                <FiClock size={14} color="ff9000" />
+                <p>8am to 6pm</p>
+              </ProvidersMeta>
+            </div>
+          </ProvidersContainer>
+        ))}
+      </ProvidersList>
     </Container>
   );
 };
