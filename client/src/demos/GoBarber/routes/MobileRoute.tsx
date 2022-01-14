013f3cac69f861e7da0406a2f53b4f37073632d9
Alex Paris <alexlima@gruposgapetro.com.br>
2022-01-13 21:41:54 -0300

    fix-client: gobarber mob route for logged user

--- client/src/demos/GoBarber/routes/MobileRoute.tsx
+++ client/src/demos/GoBarber/routes/MobileRoute.tsx
@@ -4,21 +4,33 @@ import SignIn from '../mobile/SignIn';
 import SignUp from '../mobile/SignUp';
 import Dashboard from '../mobile/Dashboard';
 
+import { useAuth } from '../hooks/context/auth';
+
 import MobilePreview from '../../../components/MobilePreview';
 import { MobileRouteProvider } from '../../../hooks/context/mobileRoute';
 
-const MobileRoute: React.FC = () => (
-  <>
-    <MobilePreview>
-      <MobileRouteProvider
-        propPages={[
-          { page: SignIn, pageTitle: 'SignIn', isActive: true },
-          { page: SignUp, pageTitle: 'SignUp' },
-          { page: Dashboard, pageTitle: 'Dashboard' },
-        ]}
-      />
-    </MobilePreview>
-  </>
-);
+interface MobilePage {
+  page: React.FC;
+  pageTitle: string;
+  isActive?: boolean;
+}
+
+const MobileRoute: React.FC = () => {
+  const { userMobile } = useAuth();
+
+  const pages: MobilePage[] = [
+    { page: SignIn, pageTitle: 'SignIn', isActive: !userMobile },
+    { page: SignUp, pageTitle: 'SignUp' },
+    { page: Dashboard, pageTitle: 'Dashboard', isActive: !!userMobile },
+  ];
+
+  return (
+    <>
+      <MobilePreview>
+        <MobileRouteProvider propPages={pages} />
+      </MobilePreview>
+    </>
+  );
+};
 
 export default MobileRoute;
